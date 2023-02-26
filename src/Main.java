f79c462a9c68d21ea5f3f189df8a66a24f758232
ZalinaZN <zaseeva1985@mail.ru>
2023-02-26 20:30:08 +0300

    commit message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] sales = new int[]{1, 5, 10, 6, 4};
+        SalesManager salesManager = new SalesManager(sales);
+        int max = salesManager.max();
+        System.out.println(max);
+
     }
 }
