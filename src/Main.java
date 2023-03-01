2317b4bc9935d32857f4e0b494b2c59ac1b0de2c
ZalinaZN <zaseeva1985@mail.ru>
2023-03-01 21:40:28 +0300

    Изменение типа продаж с int на long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        long[] sales = new long[]{100, 5000, 10000, 60000, 40000};
+        long[] sales = new long[]{100, 5000, 1000, 6000, 40000};
         SalesManager salesManager = new SalesManager(sales);
         long max = salesManager.max();
         System.out.println(max);
